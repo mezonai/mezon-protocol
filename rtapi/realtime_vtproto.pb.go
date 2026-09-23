7455daac573b5d0c22ba1626ee93197a24d61313
Nguyen Tran Nhan <nguyentran@ncc.asia>
2026-09-23 09:58:30 +0700

    add peer id to voice event

--- rtapi/realtime_vtproto.pb.go
+++ rtapi/realtime_vtproto.pb.go
@@ -5257,6 +5257,11 @@ func (m *VoiceLeavedEvent) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.PeerId != 0 {
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.PeerId))
+		i--
+		dAtA[i] = 0x28
+	}
 	if m.VoiceUserId != 0 {
 		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.VoiceUserId))
 		i--
@@ -5311,6 +5316,11 @@ func (m *VoiceJoinedEvent) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 	if m.unknownFields != nil {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
+	}
+	if m.PeerId != 0 {
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.PeerId))
+		i--
+		dAtA[i] = 0x48
 	}
 	if len(m.LastScreenshot) > 0 {
 		i -= len(m.LastScreenshot)
@@ -12292,6 +12302,9 @@ func (m *VoiceLeavedEvent) SizeVT() (n int) {
 	if m.VoiceUserId != 0 {
 		n += 1 + protohelpers.SizeOfVarint(uint64(m.VoiceUserId))
 	}
+	if m.PeerId != 0 {
+		n += 1 + protohelpers.SizeOfVarint(uint64(m.PeerId))
+	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -12330,6 +12343,9 @@ func (m *VoiceJoinedEvent) SizeVT() (n int) {
 	l = len(m.LastScreenshot)
 	if l > 0 {
 		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
+	}
+	if m.PeerId != 0 {
+		n += 1 + protohelpers.SizeOfVarint(uint64(m.PeerId))
 	}
 	n += len(m.unknownFields)
 	return n
@@ -26306,6 +26322,25 @@ func (m *VoiceLeavedEvent) UnmarshalVT(dAtA []byte) error {
 					break
 				}
 			}
+		case 5:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PeerId", wireType)
+			}
+			m.PeerId = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.PeerId |= int32(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
@@ -26574,6 +26609,25 @@ func (m *VoiceJoinedEvent) UnmarshalVT(dAtA []byte) error {
 			}
 			m.LastScreenshot = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
+		case 9:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PeerId", wireType)
+			}
+			m.PeerId = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.PeerId |= int32(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
